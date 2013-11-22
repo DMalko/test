9568dad9ad1f72561a5bedf9ee92e54cbf07472e
DMalko <bionet@inbox.ru>
2013-11-22 16:32:15 +0400

    all removed

--- orthomcl.bat
+++
@@ -1,8 +0,0 @@
-#../bin/orthomclInstallSchema ./orthomcl.config ./install_schema.log
-../bin/orthomclBlastParser ./allprots.blst ./compliantFasta >> ./similarSequences.txt
-../bin/orthomclLoadBlast ./orthomcl.config ./similarSequences.txt
-../bin/orthomclPairs ./orthomcl.config ./pairs.log cleanup=yes
-../bin/orthomclDumpPairsFiles ./orthomcl.config
-mcl ./mclInput --abc -I 1.5 -o mclOutput
-../bin/orthomclMclToGroups group_ 1 < ./mclOutput > groups.txt
-
